e2914e68cc6efa8f2c1d17086a0b1131432cf877
Leticia-SS <leticia.saraiva013@gmail.com>
2025-06-15 18:15:43 -0300

    criar multiplos logs

---
+++ AT_TourManager/Models/Logger.cs
@@ -0,0 +1,49 @@
+﻿namespace AT_TourManager.Models
+{
+    public delegate void LogDelegate(string message);
+    public class Logger
+    {
+        public static Action<string> Mensagem;
+
+        public static void LogToConsole(string message)
+        {
+            Console.WriteLine($"Log Console: {DateTime.Now} = {message}");
+        }
+
+        public static void LogToFile(string message)
+        {
+            string filePath = "log.txt";
+            using (StreamWriter writer = new StreamWriter(filePath, true))
+            {
+                writer.WriteLine($"Log Txt: {DateTime.Now} = {message}");
+            }
+        }
+
+        public static void LogToMemory(string message)
+        {
+            Console.WriteLine($"Log Memória: {DateTime.Now} = {message}");
+        }
+
+        public static void MultipleLoggers(bool logToConsole, bool logToFile, bool logToMemory)
+        {
+            if (logToConsole)
+            {
+                Mensagem += LogToConsole;
+            }
+            if (logToFile)
+            {
+                Mensagem += LogToFile;
+            }
+            if (logToMemory)
+            {
+                Mensagem += LogToMemory;
+            }
+        }
+
+
+
+
+
+
+    }
+}
---
+++ AT_TourManager/Pages/Pacotes.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AT_TourManager.Pages
+{
+    public class PacotesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
