bc056d620304cbc3712ef249ee88229d06ed7c41
Leticia-SS <leticia.saraiva013@gmail.com>
2025-06-20 13:38:34 -0300

    criando IdentityContext para login

---
+++ AT_TourManager/Data/IdentidadeContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AT_TourManager.Data
+{
+    public class IdentidadeContext : IdentityDbContext
+    {
+        public IdentidadeContext(DbContextOptions<IdentidadeContext> options) : base(options)
+        {
+        }
+    }
+}
