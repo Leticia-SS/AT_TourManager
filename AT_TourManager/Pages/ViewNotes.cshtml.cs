03258c0e0f3c4a210267b9ec37782d92eb9394a9
Leticia-SS <leticia.saraiva013@gmail.com>
2025-06-21 00:39:28 -0300

    view notes page feita

--- AT_TourManager/Pages/ViewNotes.cshtml.cs
+++ AT_TourManager/Pages/ViewNotes.cshtml.cs
@@ -5,8 +5,75 @@ namespace AT_TourManager.Pages
 {
     public class ViewNotesModel : PageModel
     {
-        public void OnGet()
+        private readonly string _filePath = Path.Combine("wwwroot", "files");
+
+        [BindProperty]
+        public string NovaNota { get; set; }
+
+        public List<string> Notas { get; set; } = new List<string>();
+        public string Mensagem { get; set; }
+
+
+        public void OnGet(string? file)
         {
+           LoadNotes();
+            if (!string.IsNullOrEmpty(file)){
+                LoadContent(file);
+            }
+        }
+
+        public IActionResult OnPost()
+        {
+            if (string.IsNullOrWhiteSpace(NovaNota))
+            {
+                Mensagem = "Nota não pode ser vazia.";
+                return Page();
+            }
+
+            SaveNote(NovaNota);
+
+            NovaNota = string.Empty;
+
+            LoadNotes();
+
+            Mensagem = "Nota salva com sucesso!";
+
+            return RedirectToPage();
+        }
+
+        private void LoadNotes()
+        {
+            if (Directory.Exists(_filePath))
+            {
+                Notas = Directory.GetFiles(_filePath, "*.txt")
+                    .Select(Path.GetFileName)
+                    .OrderByDescending(f => f)
+                    .ToList();
+            }
+        }
+
+        private void LoadContent(string fileName)
+        {
+            var filePath = Path.Combine(_filePath, fileName);
+            if (System.IO.File.Exists(filePath))
+            {
+                Mensagem = System.IO.File.ReadAllText(filePath);
+            }
+            else
+            {
+                Mensagem = "Arquivo não encontrado.";
+            }
+        }
+
+        private void SaveNote(string note)
+        {
+            if (!Directory.Exists(_filePath))
+            {
+                Directory.CreateDirectory(_filePath);
+            }
+            var fileName = $"Nota_{DateTime.Now:yyyyMMdd_HHmmss}.txt";
+            var filePath = Path.Combine(_filePath, fileName);
+            System.IO.File.WriteAllText(filePath, note);
         }
     }
 }
